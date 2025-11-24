c9437d41dfed7644f91aa18b45adfd08d310a424
sulaimon <sulaimon.gh@gmail.com>
2025-11-24 20:29:44 +0500

    Propertie & service for rooms, connection with db

---
+++ properties/rooms.py
@@ -0,0 +1,53 @@
+from utils.connection import get_db_connection
+
+
+#Create
+def create_room(room_name, description = None):
+    """
+    БД: Создание комнаты с / без описания
+    """
+    conn = get_db_connection()
+    # Проверка входных данных
+    try:
+        conn.execute("INSERT INTO rooms (room_name, description) VALUES (?, ?)", 
+                    (room_name, description))
+        conn.commit()
+    except Exception as e:
+        error = f"Произошла ошибка при создании комнаты: {e}"
+    finally:
+        conn.close()
+
+
+#Read
+def get_room_info(room_name):
+    """
+    БД: Получение данных комнаты по room_name
+    """
+    conn = get_db_connection()
+    info = None
+    # Проверка входных данных
+    try:
+        info = conn.execute(
+            "SELECT id, room_name, description FROM rooms WHERE room_name = ?", 
+            (room_name,)).fetchone()
+    except Exception as e:
+        print(e)
+        print('Ошибка')
+    finally:
+        conn.close()
+    
+    return info
+
+#Update
+def update_room_info():
+    conn = get_db_connection()
+    conn.execute("")
+    conn.commit()
+    conn.close()
+
+#Delete
+def deactivate_room():
+    conn = get_db_connection()
+    conn.execute("")
+    conn.commit()
+    conn.close()
---
+++ services/rooms.py
@@ -0,0 +1,20 @@
+from properties.rooms import create_room, get_room_info 
+
+
+def new_room(room_name, description = None):
+    create = create_room(room_name, description)
+    if not create:
+        info = get_room_info(room_name)
+        return f"Комната успешно созданна. {info}"
+    return create
+
+
+def room_info(room_name):
+    info = get_room_info(room_name)
+    return [i for i in info]
+    #description = f'"{info[1]}\"' if info[1] else 'отсутствует'
+    #return f"Название {info[0]}, описание : {description}"
+
+    
+
+
